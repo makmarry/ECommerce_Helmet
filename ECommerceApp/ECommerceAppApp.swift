85cb50cec9786402e09bfdc9af061d05df86f426
Mohak Parmar <mohak@topsinfosolutions.com>
2021-08-04 08:45:06 +0530

    Initial Commit

---
+++ ECommerceApp/ECommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceAppApp.swift
+//  ECommerceApp
+//
+//  Created by Mohak Parmar on 04/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
